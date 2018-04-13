9b53a0e2103e132bbb871341d0919e0ffef8afd6
hmanr <manriquematus1@gmail.com>
2018-04-12 22:42:09 -0600

    Diseño terminado.

--- app/src/main/java/com/example/manrique_matus/recyclerview/Adapter/SeriesAdapter.java
+++ app/src/main/java/com/example/manrique_matus/recyclerview/Adapter/SeriesAdapter.java
@@ -2,65 +2,55 @@ package com.example.manrique_matus.recyclerview.Adapter;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.manrique_matus.recyclerview.Class.Serie;
 import com.example.manrique_matus.recyclerview.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Manrique_Matus on 008/8/4/2018.
  */
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.SeriesViewHolder>{
-    private String TAG="Text";
-    ArrayList<Serie> series;
+public class SeriesAdapter extends RecyclerView.Adapter<SeriesAdapter.SeriesViewHolder>{
+    private ArrayList<Serie> series;
 
     public static class SeriesViewHolder extends RecyclerView.ViewHolder{
         CardView card;
-        TextView name,caps,desc;
+        TextView name;
         ImageView img;
-        RelativeLayout ll;
 
 
         public SeriesViewHolder(View itemView) {
             super(itemView);
-            card=(CardView)itemView.findViewById(R.id.card_view);
-            name=(TextView)itemView.findViewById(R.id.name);
-            //caps=(TextView)itemView.findViewById(R.id.chapters);
-            desc=(TextView)itemView.findViewById(R.id.desc);
-            //img=(ImageView)itemView.findViewById(R.id.img);
-            ll=itemView.findViewById(R.id.img);
+            card=itemView.findViewById(R.id.card_view);
+            name=itemView.findViewById(R.id.name);
+            img=itemView.findViewById(R.id.img);
         }
     }
 
 
-    public MyAdapter(ArrayList<Serie> series){
+    public SeriesAdapter(ArrayList<Serie> series){
         this.series=series;
     }
 
     @Override
     public SeriesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_cardview,parent,false);
-        SeriesViewHolder sv=new SeriesViewHolder(v);
-        return sv;
+        return (new SeriesViewHolder(v));
     }
 
     @Override
     public void onBindViewHolder(SeriesViewHolder holder, int position) {
         holder.name.setText(series.get(position).getName());
         //holder.caps.setText(String.valueOf(series.get(position).getCaps()));
-        holder.desc.setText(series.get(position).getDesc());
-        holder.ll.setBackgroundResource(series.get(position).getImg());
+        //holder.desc.setText(series.get(position).getDesc());
+        holder.img.setImageResource(series.get(position).getImg());
         //Log.d(TAG, "onBindViewHolder: "+series.get(position).getDesc());
 
     }
--- app/src/main/java/com/example/manrique_matus/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/manrique_matus/recyclerview/MainActivity.java
@@ -2,23 +2,19 @@ package com.example.manrique_matus.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 
-import com.example.manrique_matus.recyclerview.Adapter.MyAdapter;
+import com.example.manrique_matus.recyclerview.Adapter.SeriesAdapter;
 import com.example.manrique_matus.recyclerview.Class.Serie;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView rv;
-    MyAdapter adapter;
+    SeriesAdapter adapter;
     ArrayList<Serie> series;
-    Serie serie;
-    private String TAG = "Mensaje";
     RecyclerView.LayoutManager layoutManager;
 
     @Override
@@ -26,29 +22,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //series=serie.crearArreglo();
         series=new ArrayList<>();
 
         rv = findViewById(R.id.recycler);
         rv.setHasFixedSize(true);
 
-        layoutManager=new LinearLayoutManager(this);
+        layoutManager=new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
         rv.setLayoutManager(layoutManager);
 
         prepareSeries();
 
-        adapter=new MyAdapter(series);
+        adapter=new SeriesAdapter(series);
         rv.setAdapter(adapter);
 
 
     }
 
     public void prepareSeries(){
+        String TAG = "Mensaje";
         series=new ArrayList<>();
         series.add(new Serie("The Walking Dead","13", R.drawable.twd,"TV show created by Robert Kirgman"));
         series.add(new Serie("Game of Thrones","13",R.drawable.got,"TV show created by George R. Martin"));
         series.add(new Serie("Breaking bad","13",R.drawable.bbad,"TV show created by Vince Gilligan"));
-        rv=(RecyclerView)findViewById(R.id.recycler);
 
         Log.d(TAG, "onCreate:"+series.get(0).getName());
         Log.d(TAG, "onCreate:"+series.get(1).getName());
